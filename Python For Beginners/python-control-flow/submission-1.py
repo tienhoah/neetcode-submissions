513ba8392844029bf35269e413b21305e0d79f82
tienhoah <6288799+tienhoah@users.noreply.github.com>
2026-08-21 16:47:46 -0700

    Add: python-control-flow - submission-1

---
+++ Python For Beginners/python-control-flow/submission-1.py
@@ -0,0 +1,16 @@
+for i in range(1, 8):
+    pass
+
+if True:
+    pass
+
+def unfinsished_function():
+    pass
+
+for i in range(1, 8):
+    break
+
+for i in range(1, 8):
+    continue
+
+print("nothing else happened")
